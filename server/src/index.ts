620880cdb92e9eb6efff47c37975892b7965fd8d
Muhammad Ahmed Hassan <ah363702@gmail.com>
2022-02-22 10:01:45 +0500

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { config } from "./config/config";
+import { app } from "./app";
+
+dotenv.config();
+console.log("working");
+
+const start = async () => {
+  const { MONGO_URI, PORT } = config();
+
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Connected to db successfully");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(PORT, () => {
+    console.log("app started on port " + PORT);
+  });
+};
+
+start();
